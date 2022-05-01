141113f8dd5de75d83fa52a13a4599b73b392d11
Lucas Moraes <lucas.moraes88@yahoo.com>
2022-05-01 11:45:58 -0300

    Adjust data types

--- backend/app/model/ong.model.js
+++ backend/app/model/ong.model.js
@@ -1,52 +1,53 @@
-const DataTypes = require( "sequelize" );
-const database = require( "../config/db.config" );
+module.exports = ( sequelizeObj, Sequelize ) => {
+    const Ong = sequelizeObj.define( 'ong', {
+        id_ong: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        data_registro: {
+            type: Sequelize.DATEONLY,
+            allowNull: false
+        },
+        nome: {
+            type: Sequelize.STRING( 50 ),
+            allowNull: false
+        },
+        endereco: {
+            type: Sequelize.STRING( 255 ),
+            allowNull: false
+        },
+        complemento: {
+            type: Sequelize.STRING( 255 ),
+            allowNull: true
+        },
+        cep: {
+            type: Sequelize.STRING( 8 ),
+            allowNull: false
+        },
+        cidade: {
+            type: Sequelize.STRING( 255 ),
+            allowNull: false
+        },
+        estado: {
+            type: Sequelize.STRING( 2 ),
+            allowNull: false
 
-const Ong = database.define( 'ong', {
-    id_ong: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    data_registro: {
-        type: DataTypes.DATE,
-        allowNull: false
-    },
-    nome: {
-        type: DataTypes.STRING( 50 ),
-        allowNull: false
-    },
-    endereco: {
-        type: DataTypes.STRING( 255 ),
-        allowNull: false
-    },
-    complemento: {
-        type: DataTypes.STRING( 255 ),
-        allowNull: false
-    },
-    cep: {
-        type: DataTypes.NUMBER( 8 ),
-        allowNull: false
-    },
-    cidade: {
-        type: DataTypes.STRING( 255 ),
-        allowNull: false
-    },
-    estado: {
-        type: DataTypes.STRING( 2 ),
-        allowNull: false
+        },
+        latitude: {
+            type: Sequelize.FLOAT( 2, 10 ),
+            allowNull: false
 
-    },
-    latitude: {
-        type: DataTypes.FLOAT( 2, 10 ),
-        allowNull: false
+        },
+        longitude: {
+            type: Sequelize.FLOAT( 2, 10 ),
+            allowNull: false
 
-    },
-    longitute: {
-        type: DataTypes.FLOAT( 2, 10 ),
-        allowNull: false
+        },
+    } );
 
-    },
-} );
+    return Ong;
 
-module.exports = Ong;
+};
+
--- backend/app/model/usuario.model.js
+++ backend/app/model/usuario.model.js
@@ -1,56 +1,56 @@
-const DataTypes = require( "sequelize" );
-const database = require( "../config/db.config" );
+module.exports = ( sequelizeObj, Sequelize ) => {
+    const Usuario = sequelizeObj.define( 'usuario', {
+        id_usuario: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        data_registro: {
+            type: Sequelize.DATEONLY,
+            allowNull: false
+        },
+        nome: {
+            type: Sequelize.STRING( 50 ),
+            allowNull: false
+        },
+        sobrenome: {
+            type: Sequelize.STRING( 255 ),
+            allowNull: false
+        },
+        endereco: {
+            type: Sequelize.STRING( 255 ),
+            allowNull: false
+        },
+        complemento: {
+            type: Sequelize.STRING( 255 ),
+            allowNull: true
+        },
+        cep: {
+            type: Sequelize.STRING( 8 ),
+            allowNull: false
+        },
+        cidade: {
+            type: Sequelize.STRING( 255 ),
+            allowNull: false
+        },
+        estado: {
+            type: Sequelize.STRING( 2 ),
+            allowNull: false
 
-const Usuario = database.define( 'usuario', {
-    id_usuario: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    data_registro: {
-        type: DataTypes.DATE,
-        allowNull: false
-    },
-    nome: {
-        type: DataTypes.STRING( 50 ),
-        allowNull: false
-    },
-    sobrenome: {
-        type: DataTypes.STRING( 255 ),
-        allowNull: false
-    },
-    endereco: {
-        type: DataTypes.STRING( 255 ),
-        allowNull: false
-    },
-    complemento: {
-        type: DataTypes.STRING( 255 ),
-        allowNull: false
-    },
-    cep: {
-        type: DataTypes.NUMBER( 8 ),
-        allowNull: false
-    },
-    cidade: {
-        type: DataTypes.STRING( 255 ),
-        allowNull: false
-    },
-    estado: {
-        type: DataTypes.STRING( 2 ),
-        allowNull: false
+        },
+        latitude: {
+            type: Sequelize.FLOAT( 2, 10 ),
+            allowNull: false
 
-    },
-    latitude: {
-        type: DataTypes.FLOAT( 2, 10 ),
-        allowNull: false
+        },
+        longitude: {
+            type: Sequelize.FLOAT( 2, 10 ),
+            allowNull: false
 
-    },
-    longitute: {
-        type: DataTypes.FLOAT( 2, 10 ),
-        allowNull: false
+        },
+    } );
 
-    },
-} );
+    return Usuario;
+};
 
-module.exports = Usuario;
