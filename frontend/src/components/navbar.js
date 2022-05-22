74a9cf758640bf48d7e9537ace100d9a53abc475
Lucas Moraes <lucas.moraes88@yahoo.com>
2022-05-22 13:31:16 -0300

    Leaflet

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -2,119 +2,53 @@
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import {
-    BookmarkAltIcon,
-    CalendarIcon,
-    ChartBarIcon,
-    CursorClickIcon,
     MenuIcon,
-    PhoneIcon,
-    PlayIcon,
-    RefreshIcon,
-    ShieldCheckIcon,
-    SupportIcon,
-    ViewGridIcon,
     XIcon,
+    SearchIcon
 } from "@heroicons/react/outline";
-import { ChevronDownIcon } from "@heroicons/react/solid";
 
-const solutions = [
-    {
-        name: "Analytics",
-        description:
-            "Get a better understanding of where your traffic is coming from.",
-        href: "#",
-        icon: ChartBarIcon,
-    },
-    {
-        name: "Engagement",
-        description: "Speak directly to your customers in a more meaningful way.",
-        href: "#",
-        icon: CursorClickIcon,
-    },
-    {
-        name: "Security",
-        description: "Your customers' data will be safe and secure.",
-        href: "#",
-        icon: ShieldCheckIcon,
-    },
-    {
-        name: "Integrations",
-        description: "Connect with third-party tools that you're already using.",
-        href: "#",
-        icon: ViewGridIcon,
-    },
-    {
-        name: "Automations",
-        description:
-            "Build strategic funnels that will drive your customers to convert",
-        href: "#",
-        icon: RefreshIcon,
-    },
-];
-const callsToAction = [
-    { name: "Watch Demo", href: "#", icon: PlayIcon },
-    { name: "Contact Sales", href: "#", icon: PhoneIcon },
-];
-const resources = [
-    {
-        name: "Help Center",
-        description:
-            "Get all of your questions answered in our forums or contact support.",
-        href: "#",
-        icon: SupportIcon,
-    },
-    {
-        name: "Guides",
-        description:
-            "Learn how to maximize our platform to get the most out of it.",
-        href: "#",
-        icon: BookmarkAltIcon,
-    },
-    {
-        name: "Events",
-        description:
-            "See what meet-ups and other events we might be planning near you.",
-        href: "#",
-        icon: CalendarIcon,
-    },
-    {
-        name: "Security",
-        description: "Understand how we take your privacy seriously.",
-        href: "#",
-        icon: ShieldCheckIcon,
-    },
-];
-const recentPosts = [
-    { id: 1, name: "Boost your conversion rate", href: "#" },
-    {
-        id: 2,
-        name: "How to use search engine optimization to drive traffic to your site",
-        href: "#",
-    },
-    { id: 3, name: "Improve your customer experience", href: "#" },
-];
-
-function classNames ( ...classes ) {
-    return classes.filter( Boolean ).join( " " );
-}
+import icon from '../assets/icon.png';
 
 export default function Navbar () {
     return (
-        <Popover className="relative bg-white">
+        <Popover className="relative">
             <div className="max-w-7xl mx-auto px-4 sm:px-6">
                 <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-                    <div className="flex justify-start lg:w-0 lg:flex-1">
+
+                    <div className="flex-1 w-12 justify-center">
+                        <div className="flex">
+                            <input
+                                type="text"
+                                name="first-name"
+                                placeholder="Digite o endereço"
+                                id="first-name"
+                                autoComplete="given-name"
+                                className="focus:ring-lime-500 focus:border-lime-500 block w-60 shadow-sm sm:text-sm border-gray-300 rounded-md"
+                            />
+
+                            <button
+                                type="submit"
+                                className="ml-3 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-lime-600 hover:bg-lime-700"
+                            >
+                                <SearchIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                        </div>
+
+                    </div>
+
+                    <div className="flex justify-center lg:w-0 lg:flex-1">
                         <a href="#">
                             <span className="sr-only">Workflow</span>
                             <img
                                 className="h-8 w-auto sm:h-10"
-                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                src={ icon }
                                 alt=""
                             />
                         </a>
                     </div>
+
                     <div className="-mr-2 -my-2 md:hidden">
-                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lime-500">
                             <span className="sr-only">Open menu</span>
                             <MenuIcon className="h-6 w-6" aria-hidden="true" />
                         </Popover.Button>
@@ -124,13 +58,13 @@ export default function Navbar () {
                             href="#"
                             className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
                         >
-                            Acesso de ONG's
+                            Acesso perfil
                         </a>
                         <a
                             href="#"
-                            className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                            className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-lime-600 hover:bg-lime-700"
                         >
-                            Cadastro de ONG
+                            Cadastrar
                         </a>
                     </div>
                 </div>
@@ -155,12 +89,12 @@ export default function Navbar () {
                                 <div>
                                     <img
                                         className="h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                        src={ icon }
                                         alt="Workflow"
                                     />
                                 </div>
                                 <div className="-mr-2">
-                                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lime-500">
                                         <span className="sr-only">Close menu</span>
                                         <XIcon className="h-6 w-6" aria-hidden="true" />
                                     </Popover.Button>
@@ -171,13 +105,13 @@ export default function Navbar () {
                             <div>
                                 <a
                                     href="#"
-                                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-lime-600 hover:bg-lime-700"
                                 >
                                     Acesso ONG
                                 </a>
                                 <p className="mt-6 text-center text-base font-medium text-gray-500">
                                     Quer cadastrar sua ONG?{ " " }
-                                    <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                                    <a href="#" className="text-lime-600 hover:text-lime-500">
                                         Clique aqui
                                     </a>
                                 </p>
@@ -186,6 +120,6 @@ export default function Navbar () {
                     </div>
                 </Popover.Panel>
             </Transition>
-        </Popover>
+        </Popover >
     );
 }
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,9 +1,81 @@
+import React from "react";
 import Navbar from "../components/navbar";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import Leaflet from "leaflet";
+import myPin from "../assets/pin_my_loc.png";
+import ongPin from "../assets/pin_ongs.png";
+
+
+
+const initialPosition = { lat: -23.5503099, lng: -46.6342009 };
+
+
+const pin_my_location = new Leaflet.icon( {
+    iconUrl: myPin,
+    iconSize: [ 25, 25 ]
+} );
+
+const pin_ong_location = new Leaflet.icon( {
+    iconUrl: ongPin,
+    iconSize: [ 25, 25 ]
+} );
+
+
+
 
 export default function Home () {
+
+    const [ location, setLocation ] = React.useState( initialPosition );
+
+    React.useEffect( () => {
+
+        try
+        {
+            navigator.geolocation.getCurrentPosition(
+                ( position ) => { setLocation( { lat: position.coords.latitude, lng: position.coords.longitude } ); }
+            );
+        } catch ( error )
+        {
+            console.log( "Não pe possível localizar" );
+        }
+    }, [] );
+
     return (
         <>
             <Navbar />
+
+            <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="sm:text-center lg:text-left">
+                    <div style={ { height: "80vh" } }>
+                        {/* <MapContainer
+                            center={ [ location.lat, location.lng ] } zoom={ 12 } style={ { with: "100%", height: "100%" } }>
+                            <TileLayer
+                                url={ `https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${ process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX }` }
+                            />
+                            <Marker
+                                position={ [ location.lat, location.lng ] }
+                                icon={ pin_my_location }
+                            >
+                                <Popup>
+                                    Sua Localização
+                                </Popup>
+                            </Marker>
+
+                            <Marker
+                                key={ 2 }
+                                position={ [ -23.5808015, -46.5250603 ] }
+                                icon={ pin_ong_location }
+                            >
+                                <Popup>
+                                    OngA
+                                </Popup>
+                            </Marker>
+                        </MapContainer> */}
+                    </div>
+                </div>
+            </main>
+
+
         </>
     );
-};
+}
