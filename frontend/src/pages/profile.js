72634468676482bafae77e0fc3ea9a01a4a4c7a5
Lucas Moraes <lucas.moraes88@yahoo.com>
2022-05-24 21:23:33 -0300

    Primeira versão do profile

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,9 +1,103 @@
 import React from "react";
+import icon from "../assets/icon.png";
+import { LockClosedIcon } from "@heroicons/react/solid";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
     <>
-      <h1>Login</h1>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img className="mx-auto h-12 w-auto" src={icon} alt="Workflow" />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Acesse se já tiver cadastro
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-lime-500 focus:border-lime-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-lime-500 focus:border-lime-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-lime-600 focus:ring-lime-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Lembrar acesso
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-lime-600 hover:text-lime-500"
+                >
+                  Esqueceu a senha ?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-lime-600 hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lime-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-lime-500 group-hover:text-lime-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                Autenticar
+              </button>
+            </div>
+            <div className="flex justify-end">
+              <Link
+                to="/"
+                className="whitespace-nowrap my-3 text-base font-medium text-gray-500 hover:text-gray-900"
+              >
+                Voltar
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
